c5514ab4a0bb9b28e27d71a00998c680aec48783
Jake Hallam <jakehallam95@gmail.com>
2019-04-26 16:58:10 +0100

    created expected the interface

---
+++ src/index.js
@@ -0,0 +1,27 @@
+
+/*
+
+*/
+export function createStore(){
+
+};
+
+export const types = Object.freeze({
+
+});
+
+export function generateValues(){
+
+};
+
+export const uniqueness = Object.freeze({
+   
+});
+
+export function hasBeenChanged(){
+
+}
+
+export function createCleanedStore(){
+    
+}
